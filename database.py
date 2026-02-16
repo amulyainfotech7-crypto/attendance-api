442f75c50ce132a079915bf8b8175c9145c752b4
Amulya Rana <your_github_email@example.com>
2026-02-16 15:58:36 +0530

    Attendance server ready

--- database.py
+++ database.py
@@ -1,10 +1,15 @@
-import psycopg2
 import os
+import psycopg2
 from dotenv import load_dotenv
+from pathlib import Path
 
-load_dotenv()
+# Force absolute path
+env_path = Path(__file__).parent / ".env"
+load_dotenv(dotenv_path=env_path)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+print("DATABASE_URL LOADED:", DATABASE_URL)
 
 def connect_db():
     return psycopg2.connect(DATABASE_URL)
